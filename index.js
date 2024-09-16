ce6acb1ec2363a109595bd915cee5ab940e0fa9e
Anastasia Efremenko <nastasiya.efremenko@gmail.com>
2024-09-16 14:12:54 +0300

    Add validation

--- index.js
+++ index.js
@@ -18,60 +18,44 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-// let persons = [
-//   {
-//     id: "1",
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: "2",
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: "3",
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: "4",
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
-app.get("/info", (req, res) => {
+app.get("/info", (req, res, next) => {
   const time = new Date();
-  Person.find({}).then((persons) => {
-    res.send(
-      `<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`
-    );
-  });
+  Person.find({})
+    .then((persons) => {
+      res.send(
+        `<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then((person) => {
-    if (person) {
-      res.json(person);
-    } else {
-      res.status(404).end();
-    }
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findOneAndDelete(id).then(() => {
-    res.status(204).end();
-  });
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-app.post("/api/persons", (req, res) => {
-  //const newId = Math.floor(Math.random() * 10000);
+app.post("/api/persons", (req, res, next) => {
   const person = new Person({
     name: req.body.name,
     number: req.body.number,
@@ -84,25 +68,43 @@ app.post("/api/persons", (req, res) => {
     res.status(400).send({ error: "The number is missing" }).end();
     return;
   }
-  Person.findOne({ name: person.name }).then((found) => {
-    console.log(found);
-    if (found) {
-      res.status(403).send({ error: "name must be unique" }).end();
-    } else {
-      person.save().then((result) => {
-        res.status(200).send({ message: "Contact saved" }).end();
+  Person.findOne({ name: person.name })
+    .then((found) => {
+      if (found) {
+        res.status(403).send({ error: "name must be unique" }).end();
+      } else {
+        person
+          .save()
+          .then((result) => {
+            res.status(200).send({ message: "Contact saved" }).end();
 
-        mongoose.connection.close();
-      });
-    }
-  });
-
-  // const found = persons.find((p) => p.name === person.name);
-  // if (found) {
-  //   res.status(403).send({ error: "name must be unique" }).end();
-  // }
+            //mongoose.connection.close();
+          })
+          .catch((error) => next(error));
+      }
+    })
+    .catch((error) => next(error));
 });
+app.put("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndUpdate(id, { number: req.body.number })
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+const errorHandler = (error, req, res, next) => {
+  console.log(error.name);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: error.message });
+  } else {
+    return res.status(500).send({ error: "something went wrong" });
+  }
+};
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
